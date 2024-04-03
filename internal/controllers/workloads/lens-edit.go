4398c0d3be02f49abaaf43d549f7f13ae0069599
Sebastian Döll <sebastian@katallaxie.me>
2024-04-03 08:41:08 +0000

    wip: none of these options

--- internal/controllers/workloads/lens-edit.go
+++ internal/controllers/workloads/lens-edit.go
@@ -7,6 +7,7 @@ import (
 	"github.com/google/uuid"
 	authz "github.com/zeiss/fiber-authz"
 	"github.com/zeiss/fiber-htmx/components/buttons"
+	"github.com/zeiss/fiber-htmx/components/collapsible"
 	"github.com/zeiss/fiber-htmx/components/drawers"
 	"github.com/zeiss/fiber-htmx/components/forms"
 	"github.com/zeiss/fiber-htmx/components/menus"
@@ -145,6 +146,13 @@ type EditFormProps struct {
 func EditFormComponent(p EditFormProps) htmx.Node {
 	choices := make([]htmx.Node, len(p.Question.Choices))
 
+	var noneOfThese bool
+	for _, choice := range p.Question.Choices {
+		if choice.Ref == "none_of_these" {
+			noneOfThese = true
+		}
+	}
+
 	for _, choice := range p.Question.Choices {
 		var checked bool
 		for _, answer := range p.Answer.Choices {
@@ -153,50 +161,31 @@ func EditFormComponent(p EditFormProps) htmx.Node {
 			}
 		}
 
-		input := forms.FormControl(
-			forms.FormControlProps{},
-			forms.FormControlLabel(
-				forms.FormControlLabelProps{},
-				forms.FormControlLabelText(
-					forms.FormControlLabelTextProps{},
-					htmx.Text(choice.Title),
-				),
-				forms.Checkbox(
-					forms.CheckboxProps{
-						Name:     "choices",
-						Value:    strconv.Itoa(choice.ID),
-						Checked:  checked,
-						Disabled: p.Answer.DoesNotApply,
-					},
-					htmx.Text(choice.Title),
-				),
-			),
+		checkbox := CheckboxComponent(
+			CheckboxProps{
+				Title:    choice.Title,
+				Value:    strconv.Itoa(choice.ID),
+				Checked:  checked,
+				Disabled: (noneOfThese && choice.Ref != "none_of_these") || p.Answer.DoesNotApply,
+			},
 		)
 
-		choices = append(choices, input)
+		choices = append(choices, checkbox)
 	}
 
 	return htmx.Form(
 		htmx.HxPost(""),
 		htmx.HxSwap("none"),
 		htmx.Group(choices...),
-		forms.FormControl(
-			forms.FormControlProps{},
-			forms.FormControlLabel(
-				forms.FormControlLabelProps{},
-				forms.FormControlLabelText(
-					forms.FormControlLabelTextProps{},
-					htmx.Text("Question does not apply to this workload"),
-				),
-				forms.Toggle(
-					forms.ToggleProps{
-						Name:    "does_not_apply",
-						Value:   "1",
-						Checked: p.Answer.DoesNotApply,
-					},
-					htmx.HyperScript("on change if me.checked set disabled of <input[type=checkbox][name=choices]/> to true else set disabled of <input[type=checkbox][name=choices]/> to false"),
-				),
-			),
+		DoesNotApplyComponent(
+			DoesNotApplyProps{
+				Checked: p.Answer.DoesNotApply,
+			},
+		),
+		AdditionalInformationComponent(
+			AdditionalInformationProps{
+				Description: p.Question.Description,
+			},
 		),
 		forms.TextareaBordered(
 			forms.TextareaProps{
@@ -214,6 +203,87 @@ func EditFormComponent(p EditFormProps) htmx.Node {
 			},
 
 			htmx.Text("Save"),
+		),
+	)
+}
+
+// CheckboxProps ...
+type CheckboxProps struct {
+	Title    string
+	Value    string
+	Checked  bool
+	Ref      string
+	Disabled bool
+}
+
+// CheckboxComponent ...
+func CheckboxComponent(p CheckboxProps) htmx.Node {
+	return forms.FormControl(
+		forms.FormControlProps{},
+		forms.FormControlLabel(
+			forms.FormControlLabelProps{},
+			forms.FormControlLabelText(
+				forms.FormControlLabelTextProps{},
+				htmx.Text(p.Title),
+			),
+			forms.Checkbox(
+				forms.CheckboxProps{
+					Name:     "choices",
+					Value:    p.Value,
+					Checked:  p.Checked,
+					Disabled: p.Disabled,
+				},
+			),
+		),
+	)
+}
+
+// DoesNotApplyProps ...
+type DoesNotApplyProps struct {
+	Checked bool
+}
+
+// DoesNotApplyComponent ...
+func DoesNotApplyComponent(p DoesNotApplyProps) htmx.Node {
+	return forms.FormControl(
+		forms.FormControlProps{},
+		forms.FormControlLabel(
+			forms.FormControlLabelProps{},
+			forms.FormControlLabelText(
+				forms.FormControlLabelTextProps{},
+				htmx.Text("Question does not apply to this workload"),
+			),
+			forms.Toggle(
+				forms.ToggleProps{
+					Name:    "does_not_apply",
+					Value:   "1",
+					Checked: p.Checked,
+				},
+				htmx.HyperScript("on change if me.checked set disabled of <input[type=checkbox][name=choices]/> to true else set disabled of <input[type=checkbox][name=choices]/> to false"),
+			),
+		),
+	)
+}
+
+// AdditionalInformationProps ...
+type AdditionalInformationProps struct {
+	Description string
+}
+
+// AdditionalInformationComponent ...
+func AdditionalInformationComponent(p AdditionalInformationProps) htmx.Node {
+	return collapsible.CollapseArrow(
+		collapsible.CollapseProps{},
+		collapsible.CollapseCheckbox(
+			collapsible.CollapseCheckboxProps{},
+		),
+		collapsible.CollapseTitle(
+			collapsible.CollapseTitleProps{},
+			htmx.Text("Additional Information"),
+		),
+		collapsible.CollapseContent(
+			collapsible.CollapseContentProps{},
+			htmx.Text(p.Description),
 		),
 	)
 }
