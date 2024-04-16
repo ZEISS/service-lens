d23d7d6e6d0ca5e2f87fcdd22040e95824617ed4
Sebastian Döll <sebastian@katallaxie.me>
2024-04-16 20:14:08 +0000

    wip: profile questions

--- internal/controllers/profiles/new.go
+++ internal/controllers/profiles/new.go
@@ -202,7 +202,427 @@ func (p *ProfileNewController) Get() error {
 							cards.BodyProps{},
 							cards.Title(
 								cards.TitleProps{},
-								htmx.Text("Evaluation"),
+								htmx.Text("What is the current cloud adoption phase for the organization architecting or operating the workloads in this profile?"),
+							),
+							forms.FormControl(
+								forms.FormControlProps{},
+								forms.FormControlLabel(
+									forms.FormControlLabelProps{},
+									forms.FormControlLabelText(
+										forms.FormControlLabelTextProps{
+											ClassNames: htmx.ClassNames{
+												"-my-4": true,
+											},
+										},
+										htmx.Text("Envision Adoption Phase"),
+									),
+									forms.Radio(
+										forms.RadioProps{
+											Name:    "questions[1]",
+											Value:   "1",
+											Checked: true,
+										},
+									),
+								),
+								forms.FormControlLabel(
+									forms.FormControlLabelProps{},
+									forms.FormControlLabelText(
+										forms.FormControlLabelTextProps{
+											ClassNames: htmx.ClassNames{
+												"-my-4": true,
+											},
+										},
+										htmx.Text("Align Adoption Phase"),
+									),
+									forms.Radio(
+										forms.RadioProps{
+											Name:  "questions[1]",
+											Value: "2",
+										},
+									),
+								),
+								forms.FormControlLabel(
+									forms.FormControlLabelProps{},
+									forms.FormControlLabelText(
+										forms.FormControlLabelTextProps{
+											ClassNames: htmx.ClassNames{
+												"-my-4": true,
+											},
+										},
+										htmx.Text("Launch Adoption Phase"),
+									),
+									forms.Radio(
+										forms.RadioProps{
+											Name:  "questions[1]",
+											Value: "3",
+										},
+									),
+								),
+								forms.FormControlLabel(
+									forms.FormControlLabelProps{},
+									forms.FormControlLabelText(
+										forms.FormControlLabelTextProps{
+											ClassNames: htmx.ClassNames{
+												"-my-4": true,
+											},
+										},
+										htmx.Text("Scale Adoption Phase"),
+									),
+									forms.Radio(
+										forms.RadioProps{
+											Name:  "questions[1]",
+											Value: "4",
+										},
+									),
+								),
+								forms.FormControlLabel(
+									forms.FormControlLabelProps{},
+									forms.FormControlLabelText(
+										forms.FormControlLabelTextProps{
+											ClassNames: htmx.ClassNames{
+												"-my-4": true,
+											},
+										},
+										htmx.Text("Post-Adoption Optimization Phase"),
+									),
+									forms.Radio(
+										forms.RadioProps{
+											Name:  "questions[1]",
+											Value: "5",
+										},
+									),
+								),
+							),
+						),
+					),
+					cards.CardBordered(
+						cards.CardProps{
+							ClassNames: htmx.ClassNames{
+								"w-full": true,
+								"my-4":   true,
+							},
+						},
+						cards.Body(
+							cards.BodyProps{},
+							cards.Title(
+								cards.TitleProps{},
+								htmx.Text("What is the business value that workloads in this profile represent for your team, organization, or company?"),
+							),
+							forms.FormControl(
+								forms.FormControlProps{},
+								forms.FormControlLabel(
+									forms.FormControlLabelProps{},
+									forms.FormControlLabelText(
+										forms.FormControlLabelTextProps{
+											ClassNames: htmx.ClassNames{
+												"-my-4": true,
+											},
+										},
+										htmx.Text("Business-Critical Workloads"),
+									),
+									forms.Radio(
+										forms.RadioProps{
+											Name:    "questions[2]",
+											Value:   "1",
+											Checked: true,
+										},
+									),
+								),
+								forms.FormControlLabel(
+									forms.FormControlLabelProps{},
+									forms.FormControlLabelText(
+										forms.FormControlLabelTextProps{
+											ClassNames: htmx.ClassNames{
+												"-my-4": true,
+											},
+										},
+										htmx.Text("Strategic External-facing Workloads"),
+									),
+									forms.Radio(
+										forms.RadioProps{
+											Name:  "questions[2]",
+											Value: "2",
+										},
+									),
+								),
+								forms.FormControlLabel(
+									forms.FormControlLabelProps{},
+									forms.FormControlLabelText(
+										forms.FormControlLabelTextProps{
+											ClassNames: htmx.ClassNames{
+												"-my-4": true,
+											},
+										},
+										htmx.Text("Strategic Internal-facing Workloads"),
+									),
+									forms.Radio(
+										forms.RadioProps{
+											Name:  "questions[2]",
+											Value: "3",
+										},
+									),
+								),
+								forms.FormControlLabel(
+									forms.FormControlLabelProps{},
+									forms.FormControlLabelText(
+										forms.FormControlLabelTextProps{
+											ClassNames: htmx.ClassNames{
+												"-my-4": true,
+											},
+										},
+										htmx.Text("Internal Business Workloads"),
+									),
+									forms.Radio(
+										forms.RadioProps{
+											Name:  "questions[2]",
+											Value: "4",
+										},
+									),
+								),
+								forms.FormControlLabel(
+									forms.FormControlLabelProps{},
+									forms.FormControlLabelText(
+										forms.FormControlLabelTextProps{
+											ClassNames: htmx.ClassNames{
+												"-my-4": true,
+											},
+										},
+										htmx.Text("General Use Workloads"),
+									),
+									forms.Radio(
+										forms.RadioProps{
+											Name:  "questions[2]",
+											Value: "5",
+										},
+									),
+								),
+								forms.FormControlLabel(
+									forms.FormControlLabelProps{},
+									forms.FormControlLabelText(
+										forms.FormControlLabelTextProps{
+											ClassNames: htmx.ClassNames{
+												"-my-4": true,
+											},
+										},
+										htmx.Text("Experimentation or Testing Workloads"),
+									),
+									forms.Radio(
+										forms.RadioProps{
+											Name:  "questions[2]",
+											Value: "6",
+										},
+									),
+								),
+							),
+						),
+					),
+					cards.CardBordered(
+						cards.CardProps{
+							ClassNames: htmx.ClassNames{
+								"w-full": true,
+								"my-4":   true,
+							},
+						},
+						cards.Body(
+							cards.BodyProps{},
+							cards.Title(
+								cards.TitleProps{},
+								htmx.Text("What is the current architectural and operational lifecycle phase of the workloads in this profile?"),
+							),
+							forms.FormControl(
+								forms.FormControlProps{},
+								forms.FormControlLabel(
+									forms.FormControlLabelProps{},
+									forms.FormControlLabelText(
+										forms.FormControlLabelTextProps{
+											ClassNames: htmx.ClassNames{
+												"-my-4": true,
+											},
+										},
+										htmx.Text("Plan / Requirements Gathering / Design Phase"),
+									),
+									forms.Radio(
+										forms.RadioProps{
+											Name:    "questions[3]",
+											Value:   "1",
+											Checked: true,
+										},
+									),
+								),
+								forms.FormControlLabel(
+									forms.FormControlLabelProps{},
+									forms.FormControlLabelText(
+										forms.FormControlLabelTextProps{
+											ClassNames: htmx.ClassNames{
+												"-my-4": true,
+											},
+										},
+										htmx.Text("Development / Build / Implementation Phase"),
+									),
+									forms.Radio(
+										forms.RadioProps{
+											Name:  "questions[3]",
+											Value: "2",
+										},
+									),
+								),
+								forms.FormControlLabel(
+									forms.FormControlLabelProps{},
+									forms.FormControlLabelText(
+										forms.FormControlLabelTextProps{
+											ClassNames: htmx.ClassNames{
+												"-my-4": true,
+											},
+										},
+										htmx.Text("Testing / Pre-Production Phase"),
+									),
+									forms.Radio(
+										forms.RadioProps{
+											Name:  "questions[3]",
+											Value: "3",
+										},
+									),
+								),
+								forms.FormControlLabel(
+									forms.FormControlLabelProps{},
+									forms.FormControlLabelText(
+										forms.FormControlLabelTextProps{
+											ClassNames: htmx.ClassNames{
+												"-my-4": true,
+											},
+										},
+										htmx.Text("Deploy / Production Launch Phase"),
+									),
+									forms.Radio(
+										forms.RadioProps{
+											Name:  "questions[3]",
+											Value: "4",
+										},
+									),
+								),
+								forms.FormControlLabel(
+									forms.FormControlLabelProps{},
+									forms.FormControlLabelText(
+										forms.FormControlLabelTextProps{
+											ClassNames: htmx.ClassNames{
+												"-my-4": true,
+											},
+										},
+										htmx.Text("Maintenance / Optimization Phase"),
+									),
+									forms.Radio(
+										forms.RadioProps{
+											Name:  "questions[3]",
+											Value: "5",
+										},
+									),
+								),
+							),
+						),
+					),
+					cards.CardBordered(
+						cards.CardProps{
+							ClassNames: htmx.ClassNames{
+								"w-full": true,
+								"my-4":   true,
+							},
+						},
+						cards.Body(
+							cards.BodyProps{},
+							cards.Title(
+								cards.TitleProps{},
+								htmx.Text("What are the improvement priorities for Well-Architected Framework Reviews (WAFRs) in this profile?"),
+							),
+							forms.FormControl(
+								forms.FormControlProps{},
+								forms.FormControlLabel(
+									forms.FormControlLabelProps{},
+									forms.FormControlLabelText(
+										forms.FormControlLabelTextProps{
+											ClassNames: htmx.ClassNames{
+												"-my-4": true,
+											},
+										},
+										htmx.Text("Evaluate organizational cloud strategy and priorities"),
+									),
+									forms.Checkbox(
+										forms.CheckboxProps{
+											Name:    "questions[4]",
+											Value:   "1",
+											Checked: false,
+										},
+									),
+								),
+								forms.FormControlLabel(
+									forms.FormControlLabelProps{},
+									forms.FormControlLabelText(
+										forms.FormControlLabelTextProps{
+											ClassNames: htmx.ClassNames{
+												"-my-4": true,
+											},
+										},
+										htmx.Text("Improve operational readiness"),
+									),
+									forms.Checkbox(
+										forms.CheckboxProps{
+											Name:    "questions[4]",
+											Value:   "2",
+											Checked: true,
+										},
+									),
+								),
+								forms.FormControlLabel(
+									forms.FormControlLabelProps{},
+									forms.FormControlLabelText(
+										forms.FormControlLabelTextProps{
+											ClassNames: htmx.ClassNames{
+												"-my-4": true,
+											},
+										},
+										htmx.Text("Improve operational efficiency"),
+									),
+									forms.Checkbox(
+										forms.CheckboxProps{
+											Name:    "questions[4]",
+											Value:   "3",
+											Checked: true,
+										},
+									),
+								),
+								forms.FormControlLabel(
+									forms.FormControlLabelProps{},
+									forms.FormControlLabelText(
+										forms.FormControlLabelTextProps{
+											ClassNames: htmx.ClassNames{
+												"-my-4": true,
+											},
+										},
+										htmx.Text("Improve operational incident response"),
+									),
+									forms.Checkbox(
+										forms.CheckboxProps{
+											Name:  "questions[4]",
+											Value: "4",
+										},
+									),
+								),
+								forms.FormControlLabel(
+									forms.FormControlLabelProps{},
+									forms.FormControlLabelText(
+										forms.FormControlLabelTextProps{
+											ClassNames: htmx.ClassNames{
+												"-my-4": true,
+											},
+										},
+										htmx.Text("Improve monitoring and observability"),
+									),
+									forms.Checkbox(
+										forms.CheckboxProps{
+											Name:  "questions[4]",
+											Value: "5",
+										},
+									),
+								),
 							),
 						),
 					),
@@ -221,10 +641,20 @@ func (p *ProfileNewController) Get() error {
 							),
 						),
 					),
-					buttons.OutlinePrimary(
-						buttons.ButtonProps{},
-						htmx.Attribute("type", "submit"),
-						htmx.Text("Create Profile"),
+					cards.CardBordered(
+						cards.CardProps{
+							ClassNames: htmx.ClassNames{
+								"my-4": true,
+							},
+						},
+						cards.Body(
+							cards.BodyProps{},
+							buttons.OutlinePrimary(
+								buttons.ButtonProps{},
+								htmx.Attribute("type", "submit"),
+								htmx.Text("Create Profile"),
+							),
+						),
 					),
 				),
 			),
