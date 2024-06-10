681aff190132dd0338e7d88da1c10b438f09667c
Sebastian Döll <sebastian@katallaxie.dev>
2024-06-10 13:36:56 +0000

    chore: fix setup dev

--- scripts/postCreateCommand.sh
+++ scripts/postCreateCommand.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # This script is executed after the creation of a new project.
 
-go install github.com/oapi-codegen/oapi-codegen/v2@latest
-go install github.com/go-task/task/v3/cmd/task@latest
+go install github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen@v2.2.0
 go install github.com/air-verse/air@latest
 go install github.com/go-delve/delve/cmd/dlv@latest
