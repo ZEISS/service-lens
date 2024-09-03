f998465c2dff5b60cdfb96e98ba6b387a6123134
Sebastian Döll <sebastian@katallaxie.me>
2024-09-03 06:41:14 +0000

    chore: change to postgres

--- internal/cfg/config.go
+++ internal/cfg/config.go
@@ -8,14 +8,14 @@ import (
 type Flags struct {
 	Environment             string `envconfig:"SERVICE_LENS_ENV" default:"production"`
 	Addr                    string `envconfig:"SERVICE_LENS_ADDR" default:":8084"`
-	DatabaseURI             string `envconfig:"SERVICE_LENS_DATABASE_URI" default:"postgres://root@host.docker.internal:26257/defaultdb?sslmode=disable"`
+	DatabaseURI             string `envconfig:"SERVICE_LENS_DATABASE_URI" default:""`
 	DatabaseTablePrefix     string `envconfig:"SERVICE_LENS_DATABASE_TABLE_PREFIX" default:"service_lens_"`
-	FGAApiUrl               string `envconfig:"SERVICE_LENS_FGA_API_URL" default:"http://host.docker.internal:8080"`
+	FGAApiUrl               string `envconfig:"SERVICE_LENS_FGA_API_URL" default:""`
 	FGAStoreID              string `envconfig:"SERVICE_LENS_FGA_STORE_ID" default:""`
 	FGAAuthorizationModelID string `envconfig:"SERVICE_LENS_FGA_AUTHORIZATION_MODEL_ID" default:""`
 	OIDCIssuer              string `envconfig:"SERVICE_LENS_OIDC_ISSUER" default:""`
 	OIDCAudience            string `envconfig:"SERVICE_LENS_OIDC_AUDIENCE" default:""`
-	GitHubCallbackURL       string `envconfig:"SERVICE_LENS_GITHUB_CALLBACK_URL" default:"http://localhost:8084/auth/github/callback"`
+	GitHubCallbackURL       string `envconfig:"SERVICE_LENS_GITHUB_CALLBACK_URL" default:""`
 	GitHubClientID          string `envconfig:"SERVICE_LENS_GITHUB_CLIENT_ID" default:""`
 	GitHubClientSecret      string `envconfig:"SERVICE_LENS_GITHUB_CLIENT_SECRET" default:""`
 }
