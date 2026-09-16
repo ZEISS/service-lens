0078ae544dccdda89ae3dd5fa527d17e1b01fc92
Sebastian Döll <sebastian@katallaxie.dev>
2026-09-16 08:35:45 +0000

    fix: app config

--- src/app/(main)/auth/v2/login/page.tsx
+++ src/app/(main)/auth/v2/login/page.tsx
@@ -19,9 +19,9 @@ export default function LoginV2() {
           <p className="text-muted-foreground text-sm">Please enter your details to login.</p>
         </div>
         <div className="space-y-4">
-          {APP_CONFIG.enabledSocialProviders.microsoft && <MicrosoftButton className="w-full" />}
-          {APP_CONFIG.enabledSocialProviders.google && <GoogleButton className="w-full" />}
-          {APP_CONFIG.enabledSocialProviders.github && <GitHubButton className="w-full" />}
+          <MicrosoftButton className="w-full" />
+          <GoogleButton className="w-full" />
+          <GitHubButton className="w-full" />
           <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-border after:border-t">
             <span className="relative z-10 bg-background px-2 text-muted-foreground">Or continue with</span>
           </div>
--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -1,3 +1,4 @@
+
 import packageJson from "../../package.json"
 
 const currentYear = new Date().getFullYear()
@@ -10,10 +11,5 @@ export const APP_CONFIG = {
     title: "Service Lens - A modern-day service governance platform",
     description:
       "Service Lens is a modern-day service governance platform that helps you manage, monitor, and optimize your services with ease.",
-  },
-  enabledSocialProviders: {
-    microsoft: process.env.BETTER_AUTH_MICROSOFT_ENABLED === "true",
-    github: process.env.BETTER_AUTH_GITHUB_ENABLED === "true",
-    google: process.env.BETTER_AUTH_GOOGLE_ENABLED === "true",
-  },
+  }
 }
