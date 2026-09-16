987e0890cacd7a42fa60282f49e95907cf5b39a1
Sebastian Döll <sebastian@katallaxie.dev>
2026-09-16 08:53:17 +0000

    chore: setup database prefix

--- src/config/db-config.ts
+++ src/config/db-config.ts
@@ -1,2 +1,4 @@
+import "@/config/env-config"
+
 import "dotenv/config"
-export const DATABASE_PREFIX = process.env.DATABASE_PREFIX
+export const DATABASE_PREFIX = process.env.DATABASE_PREFIX ?? 'service_lens'
